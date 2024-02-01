4723f50f0ab11bcc907878b58b8d321d44dc5d59
unknown <tmcfall.prsvr@gmail.com>
2024-01-31 23:40:04 -0500

    js

---
+++ main.js
@@ -0,0 +1,33 @@
+let display = document.querySelector('#result')
+let buttons = document.querySelectorAll('.numbers')
+let clear = document.querySelector('#clear')
+let equal = document.querySelector('#equalTo')
+let operations = document.querySelectorAll('.operation')
+
+let firstNumber = '';
+let operation = '';
+let secondNumber = '';
+let result = ''
+
+
+function calcResult() {
+    
+  }
+
+  function operationPressed(op) {
+    
+  }
+
+  function numberPressed(number) {
+    
+  }
+
+
+function clearScreen(){
+    document.getElementById('clear').value = ""
+}
+
+function updateScreen() {
+    
+  }
+
