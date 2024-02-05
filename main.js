34554fc7bef0ef396613e9757a633ec3ca6e5999
unknown <tmcfall.prsvr@gmail.com>
2024-02-05 11:07:42 -0500

    partial

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let display = document.querySelector('#result')
-let buttons = document.querySelectorAll('.numbers')
+let number = document.querySelectorAll('.numbers')
 let clear = document.querySelector('#clear')
 let equal = document.querySelector('#equalTo')
 let operations = document.querySelectorAll('.operation')
+let decimal = document.querySelector('#decimal')
+let input = document.getElementById('inputBox');
 
 let firstNumber = '';
 let operation = '';
@@ -10,24 +12,42 @@ let secondNumber = '';
 let result = ''
 
 
+clear.addEventListener('click', clearScreen )
+
+for(let num of number){
+    num.addEventListener('click', numberPressed)}
+
+
+for(let op of operations){
+    op.addEventListener('click', operationPressed)}
+    
+
+ equal.addEventListener('click', calcResult)
+
+ decimal.addEventListener('click', decimalOp)
+
 function calcResult() {
     
   }
 
   function operationPressed(op) {
-    
+    operation = op.target.value
   }
 
   function numberPressed(number) {
     
   }
 
+  function decimalOp(){
+
+  }
 
 function clearScreen(){
     document.getElementById('clear').value = ""
 }
 
 function updateScreen() {
-    
-  }
+    display.innerText = firstNumber + operation + secondNumber
+}
+  
 
